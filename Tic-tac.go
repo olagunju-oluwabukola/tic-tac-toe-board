5c68207a66b717e03e89eb7768a30c48ad63f71c
bukola-radease <bukola.radease@gmail.com>
2025-09-26 19:49:50 +0100

    add git ignore

---
+++ Tic-tac.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+func main() {
+	//board
+    board := [] [] string{
+		{"-","-","-" },
+	 {"-","-","-" },
+	 {"-","-","-" },
+	}
+
+board [0] [0] = "x"
+board [2] [2] = "0"
+board [1] [2] = "x"
+board [1] [0] = "0"
+board [0] [2] = "x"
+
+for i := 0; i < len(board); i++{
+	fmt.Printf("%s\n", strings.Join(board[i], " "))
+}
+
+// fmt.Println(board)
+}
